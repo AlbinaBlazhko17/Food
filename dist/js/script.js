b18933e96365cddd0a07805a21ad911bee00ce81
Albinator <albinamivina@gmail.com>
2022-10-04 16:51:32 +0300

    add function hideTabContent

--- dist/js/script.js
+++ dist/js/script.js
@@ -100,6 +100,15 @@ window.addEventListener("DOMContentLoaded", () => {
   const tabs = document.querySelectorAll(".tabheader__item"),
         tabsContent = document.querySelectorAll(".tabcontent"),
         tabsParent = document.querySelector(".tabheader__items");
+
+  function hideTabContent() {
+    tabsContent.forEach(item => {
+      item.style.display = 'none';
+    });
+    tabs.forEach(item => {
+      item.classList.remove('tabheader__item_active');
+    });
+  }
 });
 
 /***/ })
