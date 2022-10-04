ff0e3bc880e85a08a537d6042b3e534e16cc17ac
Albinator <albinamivina@gmail.com>
2022-10-04 17:01:58 +0300

    add function to click on tabs

--- dist/js/script.js
+++ dist/js/script.js
@@ -110,13 +110,25 @@ window.addEventListener("DOMContentLoaded", () => {
     });
   }
 
-  function showTabContent(i) {
+  function showTabContent(i = 0) {
     tabsContent[i].style.display = 'block';
     tabs[i].classList.add('tabheader__item_active');
   }
 
   hideTabContent();
-  showTabContent(0);
+  showTabContent();
+  tabsParent.addEventListener('click', event => {
+    const target = event.target;
+
+    if (target && target.classList.contains("tabheader__item")) {
+      tabs.forEach((item, i) => {
+        if (target == item) {
+          hideTabContent();
+          showTabContent(i);
+        }
+      });
+    }
+  });
 });
 
 /***/ })
