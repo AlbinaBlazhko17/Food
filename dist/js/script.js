ae7501bbc2090db934e522078a7065393eaa37ba
Albinator <albinamivina@gmail.com>
2022-10-04 16:42:18 +0300

    install node modules and add script on site

--- dist/js/script.js
+++ dist/js/script.js
@@ -91,9 +91,12 @@
   !*** ./src/js/main.js ***!
   \************************/
 /*! no static exports found */
-/***/ (function(module, exports) {
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
 
 
+let a = 1;
 
 /***/ })
 
